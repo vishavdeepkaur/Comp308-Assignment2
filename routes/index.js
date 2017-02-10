d9c7218849e772e2bdf311d824d930a2396f09b1
vishavdeep <vishav.deep27@gmail.com>
2017-02-10 17:29:40 -0500

    created webpage

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.render('pages/index', { title: 'Express' });
-
- // res.render('index', { title: 'Express' });
-
+    res.render('pages/index', { title: 'Home' });
 });
+
+router.get('/about', function(req, res, next) {
+    res.render('pages/about', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+    res.render('pages/projects', { title: 'Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+    res.render('pages/services', { title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('pages/contact', { title: 'Contact Me' });
+});
+
+
 
 module.exports = router;
