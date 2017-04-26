cc603a3f1a2f3f007012ac716d6b5690e8e52224
vishavdeep <vishav.deep27@gmail.com>
2017-04-26 12:16:54 -0500

    mlab credentials updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
- "URI": "mongodb://localhost/businessContacts"
+ //"URI": "mongodb://localhost/businessContacts"
   //Remote MongoDB deployment -> let 
  // "URI": "mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+ "URI":"mongodb://vishavdeep:123456a@@ds013014.mlab.com:13014/businesscontacts"
 };
